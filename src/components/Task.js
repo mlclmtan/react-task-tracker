8a3a83e5896f9b9ab1ddb8c58c21dd702fedf332
Malcolm 陳策權 <malcolm@Malcolms-MacBook-Pro.local>
2021-04-20 04:24:21 +0800

    added classname with logic if reminder is true

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa'; //times=cross
 
 const Task = ({ task, onDelete, onToggle }) => { //must destructure, otherwise task.task.text
     return (
-        <div className="task" onDoubleClick={() => onToggle(task.id)}>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}> {/* if jsx have logic, should always use {} */}
             <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} /></h3>
             <p>{task.day}</p>
         </div>
