9a1efb756afc529f844cccf0dc39ea2d013265cf
Malcolm 陳策權 <malcolm@Malcolms-MacBook-Pro.local>
2021-04-20 05:11:23 +0800

    better readable, useeffect call fetch from outside

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ function App() {
 
   const [tasks, setTasks] = useState([]); //[item, mutation fun]
 
-  useEffect(()=> { //when pageload
-    const fetchTask = async () => {  //async function
-      const res = await fetch('http://localhost:5000/tasks');
-      const data = await res.json();
+  useEffect(() => {
+    //when pageload
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTask();
+      setTasks(tasksFromServer);
+    };
 
-      console.log(data)
-    }
+    getTasks(); //run function
+  }, []); //[]dependency array ???
 
-    fetchTask(); //run function
-  }, []) //[]dependency array ???
+  //Fetch Tasks
+  const fetchTask = async () => {
+    //async function
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  };
 
   //Add Task
   const addTask = (task) => {
@@ -28,26 +36,42 @@ function App() {
 
   //Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => { //every press, one item filtered (array)
-      return task.id !== id;
-    }));
+    setTasks(
+      tasks.filter((task) => {
+        //every press, one item filtered (array)
+        return task.id !== id;
+      })
+    );
   };
 
   //Toggle Reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => {
-      return task.id === id ? { ...task, reminder: !task.reminder } : task;
-    }));
+    setTasks(
+      tasks.map((task) => {
+        return task.id === id ? { ...task, reminder: !task.reminder } : task;
+      })
+    );
   };
 
   const name = 'Malcolm';
   const thisYear = new Date().getFullYear();
 
   return (
-    <div className="Container">
-      <Header myname={name} year={thisYear} showAdd={showAddTask} onAdd={() => setShowAddTask(!showAddTask)} />
-      { showAddTask && <AddTask onAdd={addTask} />} {/* if showAddTask true, show form, &&without else */}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Task to show')} {/*jsx logic inside {}, wrap html in ()*/}
+    <div className='Container'>
+      <Header
+        myname={name}
+        year={thisYear}
+        showAdd={showAddTask}
+        onAdd={() => setShowAddTask(!showAddTask)}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}{' '}
+      {/* if showAddTask true, show form, &&without else */}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Task to show'
+      )}{' '}
+      {/*jsx logic inside {}, wrap html in ()*/}
     </div>
   );
 }
