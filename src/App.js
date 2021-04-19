1036199bbaa65db49613af8e0d0d547a68c09ef8
Malcolm 陳策權 <malcolm@Malcolms-MacBook-Pro.local>
2021-04-20 04:45:05 +0800

    add new tasks into state

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ function App() {
 
   //Add Task
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = {id, ...task};
+    setTasks([...tasks, newTask]); //return new array of task
   }
 
   //Delete Task
