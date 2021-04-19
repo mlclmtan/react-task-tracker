b1d03e8b3333e90da2a53c74eb48efd6ca2e8cf0
Malcolm 陳策權 <malcolm@Malcolms-MacBook-Pro.local>
2021-04-20 03:09:30 +0800

    install react-icon restart server use delete icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,12 @@
-const Task = ({task}) => { //must destructure, otherwise task.task.text
+import { FaTimes } from 'react-icons/fa'; //times=cross
+
+const Task = ({ task }) => { //must destructure, otherwise task.task.text
     return (
         <div className="task">
-            <h3>{task.text}</h3>
+            <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} /></h3>
             <p>{task.day}</p>
         </div>
-    )
-}
+    );
+};
 
-export default Task
+export default Task;
